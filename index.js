c11693951c1caa54b3134bc49361ead1a5da6e48
David S <david.schriemer7@gmail.com>
2019-02-13 20:17:49 -0800

    Updating movement

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/move', (request, response) => {
 
   // Response data
   const data = {
-    move: 'down', // one of: ['up','down','left','right']
+    move: 'left', // one of: ['up','down','left','right']
   }
 
   return response.json(data)
